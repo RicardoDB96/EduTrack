ead57b8b77c67079b9925fe3eb7408e6decaddda
Ricardo DB <ricardo.domber96@gmail.com>
2023-11-07 14:27:40 -0600

    Creación de la DB para las Tareas

--- DB/controller.py
+++ DB/controller.py
@@ -16,6 +16,24 @@ class db_controller:
     cursor.execute(instruccion, (materia, color))
     conn.commit()
     conn.close()
+
+  def getSubjectID(materia):
+    conn = sql.connect("edutrack.db")
+    cursor = conn.cursor()
+    instruccion = f"SELECT id WHERE subject materia=?"
+    cursor.execute(instruccion, materia)
+    print(f"{cursor.fetchone()}       {cursor.fetchone()[0]}")
+    return cursor.fetchone()
+  
+  # Ingresar una tarea a la base de datos
+  def insertTask(tarea, materia_id, fecha, type):
+    createTaskTable()
+    conn = sql.connect("edutrack.db")
+    cursor = conn.cursor()
+    instruccion = f"INSERT INTO task (tarea, subject_id, fecha, type) VALUES (?, ?, ?, ?)"
+    cursor.execute(instruccion, (tarea, materia_id, fecha, type))
+    conn.commit()
+    conn.close()
   
   # Obtener los todos los datos de materias de la base de datos
   def getAllSubject():
@@ -28,15 +46,32 @@ class db_controller:
     conn.close()
     return datos
   
-# Creación de la tabla
+# Creación de la tabla de Materias
 def createSubjectTable():
     conn = sql.connect("edutrack.db")
     cursor = conn.cursor()
     cursor.execute(
       """CREATE TABLE IF NOT EXISTS subject (
         id INTEGER PRIMARY KEY,
-        materia TEXT NOT NULL,
+        materia TEXT NOT NULL UNIQUE,
         color TEXT NOT NULL
+      )"""
+    )
+    conn.commit()
+    conn.close()
+
+# Creación de la tabla de Tareas
+def createTaskTable():
+    conn = sql.connect("edutrack.db")
+    cursor = conn.cursor()
+    cursor.execute(
+      """CREATE TABLE IF NOT EXISTS Task (
+        id INTEGER PRIMARY KEY,
+        tarea TEXT NOT NULL,
+        materia_id INTEGER NOT NULL,
+        fecha TEXT NOT NULL,
+        type TEXT NOT NULL,
+        FOREIGN KEY(materia_id) REFERENCES subject(id)
       )"""
     )
     conn.commit()
